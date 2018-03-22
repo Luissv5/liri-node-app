d48325737c4ce0917f5b6d8dcd2cde47444f2501
Luis <luribesdf@gmail.com>
2018-03-22 16:32:36 -0600

    initial commit

---
+++ keys.js
@@ -0,0 +1,8 @@
+exports.twitterKeys = {
+  consumer_key: 'lul853O7KYXxkA0G5wImIQzDF',
+  consumer_secret: 'o8ohWeW1W0HpzqFZpHQjfAIzwcsCkPsuMCqJNz38vlN9fKGvEj',
+  access_token_key: '908017479888203776-9t1RnhipPaJZukBgPWvUbzaGXr67xgW',
+  access_token_secret: 'H08erU39b0lW9gT2ab3UutCp7vO40I1v4STtjZzbAATC6',
+}
+
+exports.tmdbKey = 'a8e0e0abde89beb1100c74700b2a85a1';
